c861e86d300b7f078fff47dd66ffe451780517ed
JotaMarcos <jmarcos_duarte@hotmail.com>
2022-06-17 23:33:29 -0400

    Obter, deletar e atualizar um cliente

--- src/main/java/br/com/masterbare/clientes/rest/ClienteController.java
+++ src/main/java/br/com/masterbare/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import br.com.masterbare.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -16,10 +17,49 @@ public class ClienteController {
         this.repository = repository;
     }
 
+    // Cadastar um cliente
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar( @RequestBody Cliente cliente ){
         return repository.save(cliente);
+    }
+
+
+    // Obter dados de um cliente pelo id
+    @GetMapping("{id}")
+    public  Cliente acharPorId( @PathVariable Integer id ) {
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    // Deletando um cliente
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar( @PathVariable Integer id ) {
+        repository
+                .findById(id)
+                .map( cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    }
+
+    // Atualizar um cliente
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar( @PathVariable Integer id, @RequestBody Cliente clienteAtualizado ){
+        repository
+                .findById(id)
+                .map( cliente -> {
+                    cliente.setNome(clienteAtualizado.getNome());
+                    cliente.setCpf(clienteAtualizado.getCpf());
+                   return repository.save(clienteAtualizado);
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
     }
 
 
